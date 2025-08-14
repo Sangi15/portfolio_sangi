849e021bd7c3ef7f61e04d697f21b20b47b4fb31
Sangeetha Segar <sangeethasegar989@gmail.com>
2025-08-14 16:25:33 +0530

    Build updated Flutter web app

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -33,7 +33,7 @@ const RESOURCES = {"assets/asset/files/Sangeetha_Segar_CVR.pdf": "39f9d6d1559b07
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "92f9ac229621b882898653213af4d920",
+"flutter_bootstrap.js": "6c7b659257d5e0e31992700ba7f015a7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
